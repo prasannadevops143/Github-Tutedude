a9fdbddca88f12708d73a76ce237844d198295c2
Prasanna <prasannadevops143@gmail.com>
2026-08-16 08:04:33 +0000

    Add Item ID field to To-Do Form

--- app.py
+++ app.py
@@ -1,11 +1,3 @@
-@app.route('/todo')
-def todo_page():
-    return '''
-    <form action="/submittodoitem" method="POST">
-        <label for="itemName">Item Name:</label>
-        <input type="text" id="itemName" name="itemName" required><br><br>
-        <label for="itemDescription">Item Description:</label>
-        <textarea id="itemDescription" name="itemDescription" required></textarea><br><br>
-        <button type="submit">Submit To-Do</button>
-    </form>
-    '''
+<label for="itemId">Item ID:</label>
+<input type="text" id="itemId" name="itemId"><br><br>
+
